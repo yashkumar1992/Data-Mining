f95a6ca11382bbf3032af45dcbb55da13f11efff
yashkumar1992 <yashashokgomathi@gmail.com>
2016-08-07 17:12:40 +0530

    Add files via upload

---
+++ DataProfiling.r
@@ -0,0 +1,17 @@
+# Data Profiling is the stage where you look at data and understand it.
+# It includes univariate and bivariate analysis.
+
+# Read the input file into R Data Frame
+inputData <- read.csv("inputfile.csv",sep='\t')
+
+# View the dataframe as table
+View(inputData)
+
+# Summary Statistics 
+summary(inputData)
+
+# Histogram of each parameter
+hist(inputData$x1)
+
+# Correlation Matrix
+cor(inputData)
